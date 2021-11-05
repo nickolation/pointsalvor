f071125867dab1b68a703309dd1e7ae41e1b887c
nickolation <sf1951@mail.ru>
2021-11-05 17:45:43 +0300

    Add erorr-file

---
+++ invalid.go
@@ -0,0 +1,33 @@
+package pointsalvor
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	errUrl              = errors.New("url request is empty")
+	errMethod           = errors.New("method is invalid")
+	errJsonMarshal      = errors.New("eror with marshal request body")
+	errRequestForm      = errors.New("request is invalid")
+	errRequestPerf      = errors.New("error with perform request")
+	errModelValid       = errors.New("invalid model")
+	errInvalidNameModel = fmt.Errorf("invalid name of model - empty")
+	errSwitchType       = fmt.Errorf("error with switch type")
+
+	errDecodeIf = func(e error) error {
+		return fmt.Errorf("error with decoding interface - [%v]", e)
+	}
+
+	errDecodeMap = func(e error) error {
+		return fmt.Errorf("error with decoding map - [%v]", e)
+	}
+
+	errConvertTo = func(s string) error {
+		return fmt.Errorf("invalid convertation to interface - [%s]", s)
+	}
+
+	errKnockTo = func(e error) error {
+		return fmt.Errorf("invalid knock to api - [%v]", e)
+	}
+)
